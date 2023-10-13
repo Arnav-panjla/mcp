04f0f6b61b96ca1ff9f4da52aa0034be4a2f2fe6
Arnav-panjla <arnavpanjla@gmail.com>
2023-10-13 14:10:54 +0530

    basic structure of esp code

--- servo90/servo90.ino
+++ servo90/servo90.ino
@@ -7,15 +7,30 @@ int pos = 0;    // variable to store the servo position
 // Recommended PWM GPIO pins on the ESP32 include 2,4,12-19,21-23,25-27,32-33 
 int AservoPin = 27;
 int BservoPin = 26;
+int TRR = ;
+int TLR = ;
+int BRR = ;
+int BLR = ;
 
 void setup() {
-  myservo[1].attach(AservoPin);
-  myservo[2].attach(AservoPin);   
+    Serial.begin(115200); //begin serial monitor 
+    //initialising all input output pins
+    pinMode(AservoPin,OUTPUT);
+    pinMode(BservoPin,OUTPUT);
+    pinMode(TRR,INPUT);
+    pinMode(TLR,INPUT);
+    pinMode(BRR,INPUT);
+    pinMode(BLR,INPUT);
+    myservo[1].attach(AservoPin);
+    myservo[2].attach(BservoPin);   
+
+    //setting position of all servoes to 90 initially 
+    myservo[1].write(90);
+    myservo[2].write(90);
 }
 
 void loop() {
-    //setting position of all servoes to 90
-    myservo[1].write(90);
-    myservo[2].write(90);
+
+
   
 }
